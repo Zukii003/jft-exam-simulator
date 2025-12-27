3dcb0c85d25dbc1bc0379d58c8273ebc21db307f
Zukii003 <zuki10307@gmail.com>
2025-12-28 04:29:43 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
 import App from './App.tsx'
-=======
-import App from './App-simple.tsx'
->>>>>>> 5786027a84423379d86ca0ec4f0932efebfd5ecd
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
